6206ffd2ee9c3596e0d0ecc090c5464f713f9667
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-09-30 17:11:10 -0500

    Added matconv

---
+++ src/matconv.c
@@ -0,0 +1,147 @@
+/*
+
+	matconv.c
+
+	Copyright 2019-2021 G. Adam Stanislav
+	All rights reserved
+
+	Redistribution and use in source and binary forms,
+	with or without modification, are permitted provided
+	that the following conditions are met:
+
+	1. Redistributions of source code must retain the
+	above copyright notice, this list of conditions
+	and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the
+	above copyright notice, this list of conditions and
+	the following disclaimer in the documentation and/or
+	other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the
+	names of its contributors may be used to endorse or
+	promote products derived from this software without
+	specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+	AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+	WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+	SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+	STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+	OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#define USECLIB
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <koliba.h>
+
+#define	VERSION	"v1.0.0.0"
+
+int usage(int retval) {
+	fprintf((retval) ? stderr : stdout, "Usage: matconv [-i] input [[-o] output] [-n|N] [-t|s]\n");
+	return retval;
+}
+
+int main(unsigned int argc, char *argv[]) {
+	KOLIBA_MATRIX mat, *m;
+	FILE *f;
+	unsigned int i;
+	char *input     = NULL;
+	char *output    = NULL;
+	bool  normalize = false;
+	bool  textual   = false;
+
+	if (argc < 2) return usage(1);
+
+	for (i = 1; i < argc; i++) {
+		if ((argv[i][0] == '-')
+#ifdef	_WIN32
+			|| (argv[i][0] == '/')
+#endif
+		) switch (argv[i][1]) {
+			case 'h':
+#ifdef _WIN32
+			case '?':
+#endif
+				return usage(0);
+				break;
+			case 'i':
+				if (argv[i][2]) input = argv[i]+2;
+				else if (++i < argc) input = argv[i];
+				else return usage(3);
+				break;
+			case 'N':
+				normalize = false;
+				break;
+			case 'n':
+				normalize = true;
+				break;
+			case 'o':
+				if (argv[i][2]) output = argv[i]+2;
+				else if (++i < argc) output = argv[i];
+				else return usage(3);
+				break;
+			case 's':
+				textual = false;
+				break;
+			case 't':
+				textual = true;
+				break;
+		}
+		else if (input  == NULL) input  = argv[i];
+		else if (output == NULL) output = argv[i];
+		else return usage(4);
+	}
+
+	fprintf(stderr, "matconv, " VERSION "\nCopyright 2021 G. Adam Stanislav\nAll rights reserved\n\n");
+
+	if (input == NULL) return usage(5);
+	if ((f = fopen(input, "rb")) == NULL) {
+		fprintf(stderr, "matconv: Could not open input file '%s'.\n", input);
+		return 5;
+	}
+
+	m = KOLIBA_ReadMatrixFromCompatibleOpenFile(&mat, f, NULL);
+	fclose(f);
+
+	if (m == NULL) {
+		fprintf(stderr, "matconv: '%s' does not appear to be a supported matrix file.\n", input);
+		return 6;
+	}
+
+	if (output == NULL) f = stdout;
+	else if ((f = fopen(output, "wb")) == NULL) {
+		fprintf(stderr, "matconv: Could not create output file '%s'.\n", output);
+		return 7;
+	}
+
+	if (normalize) {
+		KOLIBA_NormalizeMatrixRow(&mat.red, 1);
+		KOLIBA_NormalizeMatrixRow(&mat.green, 1);
+		KOLIBA_NormalizeMatrixRow(&mat.blue, 1);
+	}
+
+	if (textual) {
+		i = KOLIBA_WriteM34tToOpenFile(&mat, f);
+		fprintf(f, "\n# Created with matconv " VERSION "\n");
+	}
+	else i = KOLIBA_WriteMatrixToOpenFile(&mat, f);
+	if (output != NULL) fclose(f);
+	if (i) {
+		fprintf(stderr, "matconv: Failed to write matrix to output.\n");
+		return 8;
+	}
+
+	return 0;
+}
