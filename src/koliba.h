83b1b1315038f45fcc0ffebdbad72ddfcb1417f7
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-10-29 10:51:41 -0500

    Made KOLIBA_Rec2020Slut public.

--- src/koliba.h
+++ src/koliba.h
@@ -3622,6 +3622,8 @@ KLBDC unsigned int KOLIBA_GetMinorLibraryVersion(void);
 // the values to be human readable). Thankfully, the C printf and scanf
 // routines make it very easy to print and scan real values as hexadecinal
 // text.
+//
+// We call this process Koliba Marshaling.
 
 // Get the data type of a Koliba data string.
 // The string mut be at least SLTCFILEHEADERBYTES
@@ -3683,10 +3685,16 @@ KLBDC KOLIBA_CHROMAT * KOLIBA_StringToChromat(
 	const char * const string
 );
 
+KLBDC KOLIBA_SLUT * KOLIBA_MarshalSlutFromCompatibleString(KOLIBA_SLUT *sLut, const unsigned char * const string, KOLIBA_ftype *ft);
+
+
 // Text version of Koliba palette (.pltt)
 KLBDC extern const char KOLIBA_PrintPlttFormat[];
 KLBDC extern const char KOLIBA_ScanPlttFormat[];
 KLBDC extern const char KOLIBA_ScanPlttHeaderFormat[];
+
+
+KLBDC KOLIBA_SLUT * KOLIBA_MarshalSlutFromCompatibleString(KOLIBA_SLUT *sLut, const unsigned char * const string, KOLIBA_ftype *ft);
 
 
 
@@ -4232,7 +4240,6 @@ KLBHID KOLIBA_SLUT * KOLIBA_CopyColorToSlutVertices(KOLIBA_SLUT *sLut, const KOL
 // Some locally useful data.
 KLBHID extern const KOLIBA_SLUT KOLIBA_Ones;
 KLBHID extern const KOLIBA_SLUT KOLIBA_NaturalContrastSlut;
-KLBHID extern const KOLIBA_SLUT KOLIBA_Rec2020Slut;
 KLBHID extern const double KOLIBA_NaN;
 
 // And some globally useful.
@@ -4240,6 +4247,7 @@ KLBDC extern const KOLIBA_SLUT KOLIBA_NaturalFarbaContrastSlut;
 KLBDC const KOLIBA_SLUT KOLIBA_ContrastSlut;
 KLBDC const KOLIBA_SLUT KOLIBA_FarbaContrastSlut;
 KLBDC const KOLIBA_SLUT KOLIBA_SvitContrastSlut;
+KLBDC extern const KOLIBA_SLUT KOLIBA_Rec2020Slut;
 
 
 
