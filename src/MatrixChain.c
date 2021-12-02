80fa134d34c9e4c18f52b0a7c88d58a89692b635
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-12-01 18:26:54 -0600

    MatrixChain: KOLIBA file routines.

--- src/MatrixChain.c
+++ src/MatrixChain.c
@@ -18,7 +18,7 @@ int usage(char *path) {
 
 int notmat(char *path, char *file, FILE *input) {
 	fprintf(stderr, "%s: %s is not a valid matrix file\n", path, file);
-	fclose(input);
+	KOLIBA_Close(input);
 	return 3;
 }
 
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 
 	if (argc < 4) return usage(argv[0]);
 
-	if ((input = fopen(argv[2], "rb")) == NULL) {
+	if ((input = KOLIBA_OpenToRead(argv[2])) == NULL) {
 		fprintf(stderr, "%s: Could not open input file %s\n", argv[0], argv[2]);
 		return 2;
 	}
@@ -38,32 +38,32 @@ int main(int argc, char *argv[]) {
 	if (KOLIBA_ReadMatrixFromCompatibleOpenFile(&omat, input, NULL) == NULL)
 		return notmat(argv[0], argv[2], input);
 
-	fclose(input);
+	KOLIBA_Close(input);
 
 	for (i = 3; i < argc; i++) {
-		if ((input = fopen(argv[i], "rb")) == NULL) {
+		if ((input = KOLIBA_OpenToRead(argv[i])) == NULL) {
 			fprintf(stderr, "%s: Could not open modifier file %s\n", argv[0], argv[2]);
 			return 4;
 		}
 
 		if (KOLIBA_ReadMatrixFromCompatibleOpenFile(&imat, input, NULL) == NULL)
 			return notmat(argv[0], argv[2], input);
-		fclose(input);
+		KOLIBA_Close(input);
 
 		KOLIBA_MultiplyMatrices(&omat, &omat, &imat);
 	}
 
-	if ((output = fopen(argv[1], "wb")) == NULL) {
+	if ((output = KOLIBA_OpenToWrite(argv[1])) == NULL) {
 		fprintf(stderr, "%s: Could not create output file %s\n", argv[0], argv[1]);
 			return 5;
 	}
 
 	KOLIBA_WriteM34tToOpenFile(&omat, output);
 
-	fprintf(output, "\n# The Chain:\n# ==========\n#\n");
-	for (i = 3; i < argc; i++) fprintf(output, "# %s\n", argv[i]);
+	KOLIBA_Fprintf(output, "\n# The Chain:\n# ==========\n#\n");
+	for (i = 2; i < argc; i++) KOLIBA_Fprintf(output, "# %s\n", argv[i]);
 
-	fclose(output);
+	KOLIBA_Close(output);
 
 	return 0;
 }
