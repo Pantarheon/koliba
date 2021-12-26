96cd3c1e2c0b00239db2d94299d2ad0be4caf127
G. Adam Stanislav <83801431+Pantarheon@users.noreply.github.com>
2021-12-26 15:57:16 -0600

    Added KOLIBA_AngleFromFrameWithShift()

--- src/koliba.h
+++ src/koliba.h
@@ -3874,6 +3874,20 @@ KLBDC KOLIBA_ANGLE * KOLIBA_AngleFromFrame(
 	int frames		// Must be > 0
 );
 
+// The "shift" is the exponent needed to shift
+// the midpoint from 0.5 to some value m, i.e.,
+// shift = log(m)/log(0.5) = -l2(m), where l2
+// is logarithm base 2.
+//
+// While it is not enforced (since it may be called
+// millions of times in a row), 0 < shift <= 1 is
+// generally expected.
+KLBDC KOLIBA_ANGLE * KOLIBA_AngleFromFrameWithShift(
+	KOLIBA_ANGLE * kAng, int frame,
+	int frames,		// Must be > 0
+	double shift
+);
+
 #ifdef	NOKLINLIN
 #define KOLIBA_AngleSetDegrees(kAng,angle)	KOLIBA_AngleSet(kAng, angle, KAU_degrees)
 #define KOLIBA_AngleSetRadians(kAng,angle)	KOLIBA_AngleSet(kAng, angle, KAU_radians)
